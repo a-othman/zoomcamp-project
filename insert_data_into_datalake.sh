09cbdef4c67aa8166989054cd3387f78a0a198a2
ahmedothman <ahmed.n.othman01@gmail.com>
2023-04-17 15:27:55 +0200

    updated sample size

--- insert_data_into_datalake.sh
+++ insert_data_into_datalake.sh
@@ -5,8 +5,8 @@ pip3 install kaggle
 # ensure ec2 instace or your pc has an kaggle api token to be able to download data
 kaggle datasets download -d mkechinov/ecommerce-behavior-data-from-multi-category-store
 unzip ecommerce-behavior-data-from-multi-category-store.zip
-head -n 1000 2019-Nov.csv > nov-sample.csv
-head -n 1000 2019-Oct.csv > oct-sample.csv
+head -n 100000 2019-Nov.csv > nov-sample.csv
+head -n 100000 2019-Oct.csv > oct-sample.csv
 aws s3 cp ecommerce-behavior-data-from-multi-category-store.zip s3://zoomcamp
 aws s3 cp 2019-Nov.csv s3://zoomcamp
 aws s3 cp 2019-Oct.csv s3://zoomcamp
