faab312c250665cf06378a5d0c436fc91d6c6331
ahmedothman <ahmed.n.othman01@gmail.com>
2023-04-27 18:30:17 +0200

    updated bash script

--- ec2_configure.sh
+++ ec2_configure.sh
@@ -1,8 +1,16 @@
+# specific aws cli version
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.11.4.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# create json to add kaggle token inside
+touch ~/.kaggle/kaggle.json
+
 # installing pip3
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py --user
 echo "export PATH=~/.local/bin:$PATH" >> .bash_profile
-source ~/PROFILE_SCRIPT
+source ~/bash_profile
 # installing java for pyspark
 sudo amazon-linux-extras enable corretto8
 sudo yum install java-1.8.0-amazon-corretto
